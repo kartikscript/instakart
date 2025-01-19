b14347066be2058d42dced475d2f53c198ebd78d
Kartikscript <kartik.13102003@gmail.com>
2025-01-19 17:55:10 +0530

    tab update

--- src/components/CategoryTab.tsx
+++ src/components/CategoryTab.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const CategoryTab = ({title,icon,id,isActive,handleTabCategory}:{handleTabCategory:(id:string)=>void,id:string,isActive:boolean,title:string,icon:React.ReactNode}) => {
   return (
     <div onClick={()=>handleTabCategory(id)} id={id} className='relative p-2'>
-      <div className={`flex flex-col gap-1 items-center *:size-5  text-xs ${isActive ?'font-[400] opacity-100 stroke-2':'opacity-95 *:stroke-1 font-[100]'}`}>
+      <div className={`flex flex-col gap-1 items-center text-sm ${isActive ?'font-[500] opacity-100 stroke-2':'opacity-95 *:stroke-1 font-[200]'} transition-all`}>
          {icon} {title}
       </div>
-     {isActive && <div className='absolute bottom-0 left-0 translate-y-1/2 w-full h-[3px] bg-white rounded-2xl ' />}
+       <div className={`absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 ${isActive ?'w-full':'w-0'} transition-all h-[3px] bg-white rounded-2xl `} />
     </div>
   )
 }
