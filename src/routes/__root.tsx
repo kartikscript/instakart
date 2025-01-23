58d3c942a0bfec42cdaf75b65e6e5cd002a3628a
Kartikscript <kartik.13102003@gmail.com>
2025-01-23 19:34:30 +0530

    build fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import MobileNav from '@/components/MobileNav'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
