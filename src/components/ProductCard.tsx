ff2c00000c16ff169f48986c6f74e773e665d540
Kartikscript <kartik.13102003@gmail.com>
2025-01-30 14:16:07 +0530

    UI fix

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import { useToast } from "@/hooks/use-toast"
 import { Product } from './../lib/types';
 import { useGlobalContext } from "@/context/GlobalContext";
 
-const ProductCard = ({data}:{data:Product,viewAsCartProd ?: boolean}) => {
+const ProductCard = ({data,viewAsCartProd}:{data:Product,viewAsCartProd ?: boolean}) => {
 
   const {id,title,images,minimumOrderQuantity,price,weight,discountPercentage} = data
   const imageSrc = images[0]
@@ -25,20 +25,19 @@ const ProductCard = ({data}:{data:Product,viewAsCartProd ?: boolean}) => {
   const {toast} = useToast()
   const actualPrice = price/(1 - (discountPercentage/100))
   return (
-    <div className="min-w-36 gap-2 grid grid-cols-2 *:col-span-2 text-xs font-semibold p-2 bg-grey-50 dark:bg-dark-grey-50 shadow-md shadow-grey-100 dark:shadow-dark-grey-100/20 rounded-md text-black dark:text-white">
+    <div className={`min-w-36 gap-2 grid ${viewAsCartProd ? 'grid-cols-[auto_auto_1fr] grid-rows-[auto_auto_auto]' : 'grid-cols-[auto_auto]' } place-items-center justify-items-start text-xs font-semibold p-2 bg-grey-50 dark:bg-dark-grey-50 shadow-md shadow-grey-100 dark:shadow-dark-grey-100/20 rounded-md text-black dark:text-white`}>
         <img
         src={imageSrc}
         alt="product"
-        className=" size-20 mx-auto dark:bg-white/5 bg-black/5 rounded-md  object-contain"
+        className={` ${viewAsCartProd ? 'col-span-1 row-span-full' : 'col-span-2'}  size-20 mx-auto dark:bg-white/5 bg-black/5 rounded-md  object-contain`}
       />
-     <h3 className=" text-ellipsis line-clamp-2 whitespace-normal tracking-wide">{title}  </h3>
-     <p className="font-normal text-grey-150 dark:text-dark-grey-150 text-[10px]">{weight*10} g</p>
-     <div className="max-w-full flex justify-between items-center gap-2">
-      <div className="leading-none space-y-[3px] truncate">
+     <h3 className={`${viewAsCartProd ? 'row-span-1' : 'col-span-2'}  text-ellipsis line-clamp-2 whitespace-normal tracking-wide`}>{title}  </h3>
+     <p className={`${viewAsCartProd ? 'col-start-2 col-span-1 row-span-1' : 'col-span-2'} font-normal text-grey-150 dark:text-dark-grey-150 text-[10px]`}>{weight*10} g</p>
+      <div className={`leading-none space-y-[3px] truncate w-max ${viewAsCartProd && 'col-start-2 row-span-1'}  col-span-1`}>
         <h1>$ {price}</h1>
         <p className="text-grey-150 dark:text-dark-grey-150 font-normal line-through text-[10px]">$ {actualPrice.toFixed(2)}</p>
       </div>
-      <div className={`uppercase text-sm ${quantity?"bg-main text-main-50 dark:bg-dark-main dark:text-dark-main-50 ":'text-main dark:text-dark-main'} border-main dark:border-dark-main border min-w-14 *:w-full py-1 dark:font-black text-center rounded-md transition-all duration-300`}>
+      <div className={`col-span-1 ${viewAsCartProd && 'row-span-full col-start-3'} justify-self-end w-fit uppercase text-sm ${quantity?"bg-main text-main-50 dark:bg-dark-main dark:text-dark-main-50 ":'text-main dark:text-dark-main'} border-main dark:border-dark-main border min-w-14 *:w-full py-1 dark:font-black text-center rounded-md transition-all duration-300`}>
         { 
           quantity === 0 ? (
             <button onClick={() => addQuantity()}>Add</button>
@@ -64,7 +63,6 @@ const ProductCard = ({data}:{data:Product,viewAsCartProd ?: boolean}) => {
         
       </div>
      </div>
-    </div>
   )
 }
 
